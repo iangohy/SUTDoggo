a131cbf1e74c7d4052ba5a07c8bd25080e69d137
James R T <jamestiotio@gmail.com>
2021-01-29 10:07:51 +0800

    Fix some whitespace

--- Doggo_Testing/check_odrive_version.py
+++ Doggo_Testing/check_odrive_version.py
@@ -64,16 +64,17 @@ if isinstance(device, usb.core.Device):
     # Read hardware version from one-time-programmable memory (512 Byte OTP)
     otp_sector = [s for s in dfudev.sectors if s['name'] == 'OTP Memory' and s['addr'] == 0x1fff7800][0]
     otp_data = dfudev.read_sector(otp_sector)
+
     if otp_data[0] == 0:
         otp_data = otp_data[16:]
     if otp_data[0] == 0xfe:
         hw_version = (otp_data[3], otp_data[4], otp_data[5])
     else:
         hw_version = (0, 0, 0)
-
 else:
     serial_number = device.__channel__.usb_device.serial_number
     dfudev = None
+
     # Read hardware version as reported from firmware
     hw_version_major = device.hw_version_major if hasattr(device, 'hw_version_major') else 0
     hw_version_minor = device.hw_version_minor if hasattr(device, 'hw_version_minor') else 0
@@ -89,7 +90,7 @@ fw_version = (fw_version_major, fw_version_minor, fw_version_revision, fw_versio
 
 # Report and print the relevant ODrive's board version (hw_version, with an accompanying fw_version) to the terminal console or display screen
 print("Found ODrive of serial number {} with board/hardware version {} and firmware version {}{}.".format(
-                serial_number,
-                get_hw_version_string(hw_version),
-                get_fw_version_string(fw_version),
-                " in DFU mode" if dfudev is not None else ""))
+            serial_number,
+            get_hw_version_string(hw_version),
+            get_fw_version_string(fw_version),
+            " in DFU mode" if dfudev is not None else ""))
